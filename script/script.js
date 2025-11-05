c28b51826a6e270dd858e0c48611af3883050539
Raktim-b <raktimb414@gmail.com>
2025-11-06 01:02:31 +0530

    recommendation Section added

--- script/script.js
+++ script/script.js
@@ -1,42 +1,65 @@
-document.addEventListener("DOMContentLoaded", function () {});
-var swiper = new Swiper(".mySwiper", {
-  loop: true,
-  effect: "fade",
-  autoplay: {
-    delay: 6000,
-    // disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-var swiper = new Swiper(".pop-swiper", {
-  slidesPerView: 1,
-  // spaceBetween: 30,
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    576: {
-      slidesPerView: 2,
-      spaceBetween: 20,
+document.addEventListener("DOMContentLoaded", () => {
+  var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    effect: "fade",
+    autoplay: {
+      delay: 6000,
+      // disableOnInteraction: false,
     },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-    992: {
-      slidesPerView: 4,
-      spaceBetween: 40,
+  });
+  var swiperPop = new Swiper(".pop-swiper", {
+    slidesPerView: 1,
+    // spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
     },
-    1200: {
-      slidesPerView: 5,
-      spaceBetween: 50,
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1200: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
     },
-  },
-  
-});
+  });
+  var swiperRec = new Swiper(".rec-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    // autoplay: {
+    //   delay: 2500,
+    //   disableOnInteraction: false,
+    // },
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
 
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    },
+  });
+});
