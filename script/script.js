76107991538b7831b8a2fa15bf4f73905aa090ce
Raktim-b <raktimb414@gmail.com>
2025-11-05 17:20:48 +0530

    first commit

---
+++ script/script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {});
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+var swiper = new Swiper(".pop-swiper", {
+  slidesPerView: 1,
+  // spaceBetween: 30,
+  loop: true,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
