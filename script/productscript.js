494311e34fa3ffc68dba816d2024df76dc392a07
Raktim-b <raktimb414@gmail.com>
2025-11-11 14:30:03 +0530

    best sec added

--- script/productscript.js
+++ script/productscript.js
@@ -3,8 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     slidesPerView: 1,
     spaceBetween: 50,
     speed: 3000,
-    disableOnInteraction: false, // Keep autoplay running after user interaction
-    pauseOnMouseEnter: true,
+
     loop: true,
     autoplay: {
       delay: 0,
@@ -27,16 +26,40 @@ document.addEventListener("DOMContentLoaded", () => {
       },
     },
   });
-   var swiperGift = new Swiper(".swiper-gift", {
+  var swiperGift = new Swiper(".swiper-gift", {
     slidesPerView: 1,
     spaceBetween: 50,
     speed: 3000,
-    disableOnInteraction: false, // Keep autoplay running after user interaction
-    pauseOnMouseEnter: true,
     loop: true,
     autoplay: {
       delay: 0,
       reverseDirection: true,
+      pauseOnMouseEnter: true, // Pause autoplay when mouse enters the Swiper container
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    },
+  });
+  var swiperBest = new Swiper(".swiper-best", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    speed: 3000,
+    autoplay: {
+      delay: 0,
       pauseOnMouseEnter: true, // Pause autoplay when mouse enters the Swiper container
       disableOnInteraction: false,
     },
