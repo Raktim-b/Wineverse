a4ad9e46994bd839efde56076b125ef3d2ec58bc
Raktim-b <raktimb414@gmail.com>
2025-11-12 20:23:53 +0530

    contact page added

---
+++ script/contactscript.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cursor = document.querySelector(".cursor");
+  let mouseX = 0;
+  let mouseY = 0;
+  let clientX = 0;
+  let clientY = 0;
+  document.addEventListener("mousemove", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  });
+  function MouseMove() {
+    clientX += (mouseX - clientX) * 0.05;
+    clientY += (mouseY - clientY) * 0.05;
+
+    cursor.style.top = clientY + "px";
+    cursor.style.left = clientX + "px";
+
+    requestAnimationFrame(MouseMove);
+  }
+  MouseMove();
+});
